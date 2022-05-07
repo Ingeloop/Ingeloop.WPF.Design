03fd2f39a071b1d2dd1f75f7a1202ddd53bae1c8
Ingeloop <leo.salvador@ingeloop.com>
2022-05-07 09:13:09 +0200

    Add side TabControl

---
+++ Ingeloop.WPF.Design/Controls/TabControls/SideTabControl.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+
+namespace Ingeloop.WPF.Design
+{
+    public class SideTabControl : TabControl
+    {
+        public static readonly DependencyProperty SidePanelWidthProperty = DependencyProperty.Register(nameof(SidePanelWidth), typeof(GridLength), typeof(SideTabControl),
+            new PropertyMetadata(new GridLength(120)));
+
+        public GridLength SidePanelWidth
+        {
+            get { return (GridLength)GetValue(SidePanelWidthProperty); }
+            set { SetValue(SidePanelWidthProperty, value); }
+        }
+
+        public SideTabControl()
+        {
+        }
+    }
+}
---
+++ Ingeloop.WPF.Design/Controls/TabControls/SideTabItem.cs
@@ -0,0 +1,27 @@
+﻿using Ingeloop.WPF.Icons;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+
+namespace Ingeloop.WPF.Design
+{
+    public class SideTabItem : TabItem
+    {
+        public static readonly DependencyProperty IconKindProperty = DependencyProperty.Register(nameof(IconKind), typeof(PackIconKind), typeof(SideTabItem),
+            new PropertyMetadata(PackIconKind.PlusBold));
+
+        public PackIconKind IconKind
+        {
+            get { return (PackIconKind)GetValue(IconKindProperty); }
+            set { SetValue(IconKindProperty, value); }
+        }
+
+        public SideTabItem()
+        {
+        }
+    }
+}
